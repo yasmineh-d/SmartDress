5adcc70672a0701d8c23d5f06cd2973a8868d69d
yasmineh-d <yassminehadad75@gmail.com>
2026-03-27 12:55:44 +0000

    ADD

--- smartdress/app/Models/User.php
+++ smartdress/app/Models/User.php
@@ -61,4 +61,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Favoris::class);
     }
+
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class, 'role_user');
+    }
 }
--- smartdress/resources/views/welcome.blade.php
+++ smartdress/resources/views/welcome.blade.php
@@ -21,6 +21,9 @@
     </head>
     <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
         <header class="w-full lg:max-w-4xl max-w-[335px] text-sm mb-6 not-has-[nav]:hidden">
+            <div class="bg-green-100 text-green-800 rounded-xl p-3 mb-3 text-center">
+                <strong>Debug:</strong> Hello World (mobile test message displayed).
+            </div>
             @if (Route::has('login'))
                 <nav class="flex items-center justify-end gap-4">
                     @auth
